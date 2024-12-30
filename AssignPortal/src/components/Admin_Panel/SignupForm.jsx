cb08873698199552ffbbd8a7276be15adaadfa69
321Ashutosh <108077736+321Ashutosh@users.noreply.github.com>
2024-12-30 14:38:41 +0530

    Update SignupForm.jsx

--- AssignPortal/src/components/Admin_Panel/SignupForm.jsx
+++ AssignPortal/src/components/Admin_Panel/SignupForm.jsx
@@ -1,11 +1,11 @@
 
 import axios from "axios";
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";  // Import useNavigate
+import { useNavigate } from "react-router-dom";  
 import CustomAlert from "../CustomAlert";
 
 const SignupForm = () => {
-  const navigate = useNavigate(); // Hook to navigate programmatically
+  const navigate = useNavigate(); 
   const [userName,setUserName] = useState();
   const [userPassword,setUserPassword] = useState();
   const [notificationShown, setNotificationShown] = useState(false);
@@ -13,7 +13,7 @@ const SignupForm = () => {
 
   const handleSubmit = async(e) => {
     e.preventDefault();
-    // After the signup form is submitted, redirect to the login page
+   
     try {
       const url = "http://localhost:5000/api/users/register";
       const data = {
@@ -33,26 +33,14 @@ const SignupForm = () => {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-blue-400">
-      {/* Signup Form */}
+   
       {notificationShown && <CustomAlert message={notificationMessage} onClose={()=>setNotificationShown(false)}/>}
       
       <div className="bg-white p-16 rounded shadow-2xl w-2/3">
         <h2 className="text-3xl font-bold mb-10 text-gray-800">Admin Create Your Account</h2>
 
         <form className="space-y-5" onSubmit={handleSubmit}>
-          {/* User ID Field */}
-          {/* <div>
-            <label className="block mb-1 font-bold text-gray-500">User ID</label>
-            <input
-              type="text"
-              value={userName}
-              onChange={(e)=>setUserName(e.target.value)}
-              placeholder="Enter your unique user ID"
-              className="w-full border-2 border-gray-200 p-3 rounded outline-none focus:border-purple-500"
-            />
-          </div> */}
-
-          {/* Username Field */}
+       
           <div>
             <label className="block mb-1 font-bold text-gray-500">Username</label>
             <input
@@ -64,7 +52,6 @@ const SignupForm = () => {
             />
           </div>
 
-          {/* Password Field */}
           <div>
             <label className="block mb-1 font-bold text-gray-500">Password</label>
             <input
@@ -76,7 +63,7 @@ const SignupForm = () => {
             />
           </div>
 
-          {/* Agreement Checkbox */}
+        
           <div className="flex items-center">
             <input type="checkbox" id="agree" />
             <label htmlFor="agree" className="ml-2 text-gray-700 text-sm">
@@ -84,13 +71,13 @@ const SignupForm = () => {
             </label>
           </div>
 
-          {/* Submit Button */}
+         
           <button className="block w-full bg-yellow-400 hover:bg-yellow-300 p-4 rounded text-yellow-900 hover:text-yellow-800 transition duration-300">
             Sign Up
           </button>
         </form>
 
-        {/* Link to Login */}
+       
         <p className="text-center mt-4 text-sm text-gray-700">
           Already have an account?{" "}
           <button
